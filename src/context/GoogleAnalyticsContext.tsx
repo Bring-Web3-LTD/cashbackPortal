31f3da6f641ae1af042632a5a7034d10b65133fe
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2025-09-18 16:36:56 +0300

    fix: parentLocation missing on page_view; add: new campaign retailers

--- src/context/GoogleAnalyticsContext.tsx
+++ src/context/GoogleAnalyticsContext.tsx
@@ -101,9 +101,6 @@ export const GoogleAnalyticsProvider: FC<Props> = ({ measurementId, children, pl
     }, []);
 
     useEffect(() => {
-        if (window.origin.includes('localhost')) {
-            return
-        }
 
         if (effectRan.current === location) return
 
@@ -111,9 +108,12 @@ export const GoogleAnalyticsProvider: FC<Props> = ({ measurementId, children, pl
             pageLocation: window.location.href,
             pagePath: window.location.pathname,
             pageTitle: document.title,
-            parentLocation: location
+            parentLocation: window.parent?.location?.href || 'N/A'
         }
-
+        console.log('BRING: Sending page_view event', details)
+        if (window.origin.includes('localhost')) {
+            return
+        }
         sendBackendEvent('page_view', {
             category: 'system',
             details
