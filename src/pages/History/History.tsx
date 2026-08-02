555e01803a09f5ed021a89a847bc341a28ac646a
AyalaGarbuz <ayalag@bring.network>
2026-08-02 16:48:18 +0300

    more fixes

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -55,7 +55,7 @@ const Row = ({ isActive, toggleFn, imgSrc, imgSrcFallback, status, tokenAmount, 
                             :
                             <img
                                 style={{ height: `${status.toLowerCase() === 'claimed' ? 'auto' : '100%'}` }}
-                                className={styles.img}
+                                className={`${styles.img} ${status.toLowerCase() === 'claimed' ? styles.img_claim : ''}`}
                                 src={imgSrc}
                                 alt="logo"
                                 onError={(e) => {
